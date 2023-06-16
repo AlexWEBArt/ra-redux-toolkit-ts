9a59aff02d81c5bc86e196d37684bc17de01f28d
AlexWebArt <elusiv@mail.ru>
2023-06-16 23:03:25 +0600

    fixed ssl reqest

--- src/redux/thunks/filmCardThunk.ts
+++ src/redux/thunks/filmCardThunk.ts
@@ -3,7 +3,7 @@ import { FilmCard } from "../slices/filmsSlice";
 
 export const filmCard = createAsyncThunk<FilmCard, string, { rejectValue: string }>('filmCard', async (params, { rejectWithValue }) => {
 
-    const response = await fetch(`http://www.omdbapi.com/?apikey=6ee14aa5${params}`);
+    const response = await fetch(`https://www.omdbapi.com/?apikey=6ee14aa5${params}`);
 
     if (!response.ok) {
         return rejectWithValue('Server Error!')
--- src/redux/thunks/filmsListThunk.ts
+++ src/redux/thunks/filmsListThunk.ts
@@ -10,7 +10,7 @@ export type FilmsResponse = {
 
 export const filmsList = createAsyncThunk<FilmsResponse, string, { rejectValue: string }>('films', async (params, { rejectWithValue }) => {
 
-    const response = await fetch(`http://www.omdbapi.com/?apikey=6ee14aa5${params}`);
+    const response = await fetch(`https://www.omdbapi.com/?apikey=6ee14aa5${params}`);
 
     if (!response.ok) {
         return rejectWithValue('Server Error!')
